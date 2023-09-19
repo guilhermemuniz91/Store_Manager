bc6a19ef8dcd153c44ce5a48aac5cab238406546
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-09-19 17:01:01 -0300

    req 4 parcialmente feito, faltando os testes

--- backend/src/routes/productsRouter.js
+++ backend/src/routes/productsRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { validateNewProduct } = require('../middlewares/validateNewProduct');
 const productController = require('../controllers/products.controller');
 
 const productsRouter = express.Router();
 
 productsRouter.get('/', productController.readAllProducts);
 productsRouter.get('/:id', productController.readProductsById);
-productsRouter.post('/', productController.createProduct);
+productsRouter.post('/', validateNewProduct, productController.createProduct);
     
 module.exports = productsRouter;
--- backend/src/services/validations/schema.js
+++
@@ -1,7 +0,0 @@
-const Joi = require('joi');
-
-const addNameSchema = Joi.object({
-  name: Joi.string().min(3),
-});
-
-module.exports = { addNameSchema };
